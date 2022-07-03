0ca4673a3f48e867ce7a258a6e5a93469cef4f99
Hitesh Kumar <hkhiteshkumar66@gmail.com>
2022-07-03 17:21:44 +0530

    Portfolio

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -18,25 +18,25 @@ function Head() {
                 <li className='inline list-none pr-4'>
                     <div className='flex items-center'>
                         
-                        <a href="https://twitter.com/hitesh091895" className="inline-block font-semibold text-xs text-gray-100 hover:text-black"><img src="src\icon\twitter.svg" className='min-w-6 w-6' /></a>
+                        <a href="https://twitter.com/hitesh091895" className="inline-block font-semibold text-xs text-gray-100 hover:text-black"><img src="./src/icon/twitter.svg" className='min-w-6 w-6' /></a>
                     </div>
                 </li>
                 <li className='inline list-none pr-4'>
                     <div className='flex items-center'>
                         
-                        <a href="https://github.com/kumarhites/" className="inline-block font-semibold text-xs text-gray-600 hover:text-black"><img src="src\icon\github.svg" className='min-w-6 w-6' /></a>
+                        <a href="https://github.com/kumarhites/" className="inline-block font-semibold text-xs text-gray-600 hover:text-black"><img src="./src/icon/github.svg" className='min-w-6 w-6' /></a>
                     </div>
                 </li>
                 <li className='inline list-none pr-4'>
                     <div className='flex items-center'>
                         
-                        <a href="https://www.linkedin.com/in/hitesh-kumar09/" className="inline-block font-semibold text-xs text-gray-600 hover:text-black"><img src="src\icon\linkedin.svg" className='min-w-6 w-6' /></a>
+                        <a href="https://www.linkedin.com/in/hitesh-kumar09/" className="inline-block font-semibold text-xs text-gray-600 hover:text-black"><img src="./src/icon/linkedin.svg" className='min-w-6 w-6' /></a>
                     </div>
                 </li>
                 <li className='inline list-none pr-4'>
                     <div className='flex items-center'>
                         
-                        <a href="mailto:hkhiteshkumar66@gmail.com" className="inline-block font-semibold text-xs text-gray-600 hover:text-black"><img src="src\icon\gmail.svg" className='min-w-6 w-6' /></a>
+                        <a href="mailto:hkhiteshkumar66@gmail.com" className="inline-block font-semibold text-xs text-gray-600 hover:text-black"><img src="./src/icon/gmail.svg" className='min-w-6 w-6' /></a>
                     </div>
                 </li>
             </ul>
