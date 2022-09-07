8a0fe8504bb8cf214d9a5480c8eb9b3aea35399d
Hitesh Kumar <hkhiteshkumar66@gmail.com>
2022-09-07 23:30:50 +0530

    added projects db module

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import project from "../imgs/project.png"
 import github from '../icon/github.svg'
 import navigate from '../icon/navigate.png'
 import home from '../icon/home.svg'
+import projectsData from "../projectsData/data"
 function Projects() {
+
+  const [data] = useState(projectsData());
+
+
+
   return (
   <>
     <div className="block mb-6 md:flex">
@@ -24,34 +30,123 @@ function Projects() {
             <p className='text-gray-100'>Check out a few of the projects I have worked on.</p>
         </div>
     </div>
-    <div className="block pt-12 md:flex">
-        <div className="pb-6 md:p-0 md:w-full md:max-w-[150px]">
-          <h2 className="font-xs font-light text-gray-100 uppercase tracking-widest text-sm">
-            Mark 1
-          </h2>
-        </div>
-        <div className="flex-none text-lg text-gray-100 font-light md:flex-1 md:pl-20">
-          <div className="mb-6">
-            <h3 className="font-semibold text-gray-100 pb-1 hover:underline">
-              <a href="https://replit.com/@HiteshKumar17/mark1#index.js" target="_blank">How well do you know me?</a>
-            </h3>
-            <p className="text-md text-gray-300 text-light">A CLI app built with nodeJS. You can send this to your friends and find out who knows you better</p>
+    <div className="map">
+      {data.map((d) => (
+        <div className="block pt-12 md:flex" key={d.id}>
+          <div className="pb-6 md:p-0 md:w-full md:max-w-[150px]">
+            <h2 className="font-xs font-light text-gray-100 uppercase tracking-widest text-sm">
+              {d.id}
+            </h2>
           </div>
-          <ul className='mt-6 uppercase tracking-wider flex'>
-            <li className='inline list-none pr-4'>
-              <div className='flex items-center'>
-                  <a href="https://github.com/kumarhites/mark1" className="inline-block font-semibold text-xs text-gray-100 transform transistion-all duration-150 hover:scale-125" target="_blank"><img src={github} className='min-w-6 w-6'/></a>
-              </div>
-            </li>
-            <li className='inline list-none pr-4'>
-              <div className='flex items-center'>
-                  <a href="https://replit.com/@HiteshKumar17/mark1#index.js" className="inline-block font-semibold text-xs text-gray-600 transform transistion-all duration-150 hover:scale-125" target="_blank"><img src={navigate} className='min-w-6 w-6'/></a>
-              </div>
-            </li>
-          </ul>
+          <div className="flex-none text-lg text-gray-100 font-light md:flex-1 md:pl-20">
+            <div className="mb-6">
+              <h3 className="font-semibold text-gray-100 pb-1 hover:underline">
+                <a href={d.liveLink} target="_blank">{d.projectName}</a>
+              </h3>
+              <p className="text-md text-gray-300 text-light">
+                {d.info}
+              </p>
+            </div>
+            <ul className='mt-6 uppercase tracking-wider flex'>
+              <li className='inline list-none pr-4'>
+                <div className='flex items-center'>
+                    <a href={d.githubLink} className="inline-block font-semibold text-xs text-gray-100 transform transistion-all duration-150 hover:scale-125" target="_blank"><img src={github} className='min-w-6 w-6'/></a>
+                </div>
+              </li>
+              <li className='inline list-none pr-4'>
+                <div className='flex items-center'>
+                    <a href={d.liveLink} className="inline-block font-semibold text-xs text-gray-600 transform transistion-all duration-150 hover:scale-125" target="_blank"><img src={navigate} className='min-w-6 w-6'/></a>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>  
-    <div className="block pt-12 md:flex">
+      ))}
+    </div>
+  </>
+    /* <div className="block mb-6 md:flex">
+      <a href="/" className="md:flex justify-center items-center p-2 rounded-md hover:bg-slate-800 transistion-all duration-300">
+      <img src={home} alt="home"/>
+      <p className='text-slate-50 font-bold leading-tight ml-3'>Home</p></a>
+    </div>
+    <div className='block mb-6 md:flex'>
+        <div className="items-center md:w-full md:max-w-[150px]">
+          <a aria-current="page">
+              <img src={project} className='rounded-full transform transistion-all duration-150 hover:scale-105' height="150px" width="150"/>
+          </a>
+        </div>
+        <div className="flex-none pt-6 md:pt-1 md:flex-1 md:pl-20">
+            <h1 className='text-5xl text-slate-50 font-bold leading-tight'>
+                <a aria-current="page" className="cursor-default">neoG Camp Projects</a>
+            </h1>
+            <p className='text-gray-100'>Check out a few of the projects I have worked on.</p>
+        </div>
+    </div> */
+    /* mark 1 */
+    /* <div className="block pt-12 md:flex">
+      {data.map((d) => {
+        <>
+          <div className="pb-6 md:p-0 md:w-full md:max-w-[150px]">
+        <h2 className="font-xs font-light text-gray-100 uppercase tracking-widest text-sm">
+          {d.id}
+        </h2>
+      </div>
+      <div className="flex-none text-lg text-gray-100 font-light md:flex-1 md:pl-20">
+        <div className="mb-6">
+          <h3 className="font-semibold text-gray-100 pb-1 hover:underline">
+            <a href="https://replit.com/@HiteshKumar17/mark1#index.js" target="_blank">{d.projectName}</a>
+          </h3>
+          <p className="text-md text-gray-300 text-light">A CLI app built with nodeJS. You can send this to your friends and find out who knows you better</p>
+        </div>
+        <ul className='mt-6 uppercase tracking-wider flex'>
+          <li className='inline list-none pr-4'>
+            <div className='flex items-center'>
+                <a href="https://github.com/kumarhites/mark1" className="inline-block font-semibold text-xs text-gray-100 transform transistion-all duration-150 hover:scale-125" target="_blank"><img src={github} className='min-w-6 w-6'/></a>
+            </div>
+          </li>
+          <li className='inline list-none pr-4'>
+            <div className='flex items-center'>
+                <a href="https://replit.com/@HiteshKumar17/mark1#index.js" className="inline-block font-semibold text-xs text-gray-600 transform transistion-all duration-150 hover:scale-125" target="_blank"><img src={navigate} className='min-w-6 w-6'/></a>
+            </div>
+          </li>
+        </ul>
+      </div>
+        </>
+      })}
+    </div>
+    </> */
+    /* mark 1 */
+    /* mark 1 */
+    /* <div className="block pt-12 md:flex">
+      <div className="pb-6 md:p-0 md:w-full md:max-w-[150px]">
+        <h2 className="font-xs font-light text-gray-100 uppercase tracking-widest text-sm">
+          Mark 1
+        </h2>
+      </div>
+      <div className="flex-none text-lg text-gray-100 font-light md:flex-1 md:pl-20">
+        <div className="mb-6">
+          <h3 className="font-semibold text-gray-100 pb-1 hover:underline">
+            <a href="https://replit.com/@HiteshKumar17/mark1#index.js" target="_blank">How well do you know me?</a>
+          </h3>
+          <p className="text-md text-gray-300 text-light">A CLI app built with nodeJS. You can send this to your friends and find out who knows you better</p>
+        </div>
+        <ul className='mt-6 uppercase tracking-wider flex'>
+          <li className='inline list-none pr-4'>
+            <div className='flex items-center'>
+                <a href="https://github.com/kumarhites/mark1" className="inline-block font-semibold text-xs text-gray-100 transform transistion-all duration-150 hover:scale-125" target="_blank"><img src={github} className='min-w-6 w-6'/></a>
+            </div>
+          </li>
+          <li className='inline list-none pr-4'>
+            <div className='flex items-center'>
+                <a href="https://replit.com/@HiteshKumar17/mark1#index.js" className="inline-block font-semibold text-xs text-gray-600 transform transistion-all duration-150 hover:scale-125" target="_blank"><img src={navigate} className='min-w-6 w-6'/></a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div> */
+    /* mark 1 */
+    /* mark 2 */
+    /* <div className="block pt-12 md:flex">
         <div className="pb-6 md:p-0 md:w-full md:max-w-[150px]">
           <h2 className="font-xs font-light text-gray-100 uppercase tracking-widest text-sm">
             Mark 2
@@ -77,8 +172,10 @@ function Projects() {
             </li>
           </ul>
         </div>
-      </div>  
-    <div className="block pt-12 md:flex">
+      </div>   */
+    /* mark 2 */
+    /* mark 3, 4, 5 */
+    /* <div className="block pt-12 md:flex">
         <div className="pb-6 md:p-0 md:w-full md:max-w-[150px]">
           <h2 className="font-xs font-light text-gray-100 uppercase tracking-widest text-sm">
             Mark 3
@@ -110,8 +207,10 @@ function Projects() {
             </li>
           </ul>
         </div>
-      </div>  
-    <div className="block pt-12 md:flex">
+      </div>   */
+      /* mark 3, 4, 5 */
+      /* mark 6 */
+    /* <div className="block pt-12 md:flex">
       <div className="pb-6 md:p-0 md:w-full md:max-w-[150px]">
         <h2 className="font-xs font-light text-gray-100 uppercase tracking-widest text-sm">
           Mark 6
@@ -137,8 +236,10 @@ function Projects() {
           </li>
         </ul>
       </div>
-    </div>  
-    <div className="block pt-12 md:flex">
+    </div>   */
+    /* mark 6 */
+    /* mark 7 */
+    /* <div className="block pt-12 md:flex">
       <div className="pb-6 md:p-0 md:w-full md:max-w-[150px]">
         <h2 className="font-xs font-light text-gray-100 uppercase tracking-widest text-sm">
           Mark 7
@@ -164,8 +265,10 @@ function Projects() {
           </li>
         </ul>
       </div>
-    </div>  
-    <div className="block pt-12 md:flex">
+    </div>   */
+    /* mark 7 */
+    /* mark 8 */
+    /* <div className="block pt-12 md:flex">
       <div className="pb-6 md:p-0 md:w-full md:max-w-[150px]">
         <h2 className="font-xs font-light text-gray-100 uppercase tracking-widest text-sm">
           Mark 8
@@ -191,8 +294,10 @@ function Projects() {
           </li>
         </ul>
       </div>
-    </div>  
-    <div className="block pt-12 md:flex">
+    </div>   */
+    /* mark 8 */
+    /* mark 9 */
+    /* <div className="block pt-12 md:flex">
       <div className="pb-6 md:p-0 md:w-full md:max-w-[150px]">
         <h2 className="font-xs font-light text-gray-100 uppercase tracking-widest text-sm">
           Mark 9
@@ -218,8 +323,10 @@ function Projects() {
           </li>
         </ul>
       </div>
-    </div>  
-    <div className="block pt-12 md:flex">
+    </div>   */
+    /* mark 9 */
+    /* mark 10 */
+    /* <div className="block pt-12 md:flex">
       <div className="pb-6 md:p-0 md:w-full md:max-w-[150px]">
         <h2 className="font-xs font-light text-gray-100 uppercase tracking-widest text-sm">
           Mark 10
@@ -245,8 +352,10 @@ function Projects() {
           </li>
         </ul>
       </div>
-    </div>  
-    <div className="block pt-12 md:flex">
+    </div>   */
+    /* mark 10 */
+    /* mark 11 */
+    /* <div className="block pt-12 md:flex">
       <div className="pb-6 md:p-0 md:w-full md:max-w-[150px]">
         <h2 className="font-xs font-light text-gray-100 uppercase tracking-widest text-sm">
           Mark 11
@@ -272,8 +381,10 @@ function Projects() {
           </li>
         </ul>
       </div>
-    </div>  
-    <div className="block pt-12 md:flex">
+    </div>   */
+    /* mark 11 */
+    /* mark 12 */
+    /* <div className="block pt-12 md:flex">
       <div className="pb-6 md:p-0 md:w-full md:max-w-[150px]">
         <h2 className="font-xs font-light text-gray-100 uppercase tracking-widest text-sm">
           Mark 12
@@ -299,8 +410,10 @@ function Projects() {
           </li>
         </ul>
       </div>
-    </div>  
-    <div className="block pt-12 md:flex">
+    </div>   */
+    /* mark 12 */
+    /* mark 13 */
+    /* <div className="block pt-12 md:flex">
       <div className="pb-6 md:p-0 md:w-full md:max-w-[150px]">
         <h2 className="font-xs font-light text-gray-100 uppercase tracking-widest text-sm">
           Mark 13
@@ -326,8 +439,10 @@ function Projects() {
           </li>
         </ul>
       </div>
-    </div>  
-    <div className="block pt-12 md:flex">
+    </div>   */
+    /* mark 13 */
+    /* mark 14 */
+    /* <div className="block pt-12 md:flex">
       <div className="pb-6 md:p-0 md:w-full md:max-w-[150px]">
         <h2 className="font-xs font-light text-gray-100 uppercase tracking-widest text-sm">
           Mark 14
@@ -353,8 +468,8 @@ function Projects() {
           </li>
         </ul>
       </div>
-    </div>  
-  </>
+    </div>   */
+    /* mark 14 */
   )
 }
 
---
+++ src/projectsData/data.js
@@ -0,0 +1,88 @@
+export default function projectsData(){
+    return [
+        {
+            id: "Mark 1",
+            projectName: "How well do you know me?",
+            info: "A CLI app built with nodeJS. You can send this to your friends and find out who knows you better.",
+            githubLink: "https://github.com/kumarhites/mark1",
+            liveLink: ""
+        },
+        {
+            id: "Mark 2",
+            projectName: "Ocean Quiz",
+            info: "A small fun quiz to know, how well do you know about our ocean life!",
+            githubLink: "www.github.com",
+            liveLink: "https://replit.com/@HiteshKumar17/mark2#index.js"
+        },
+        {
+            id: "Mark 3, 4, 5",
+            projectName: "Personal Portfolio",
+            info: "My Personal Portfolio developed using React HTML, CSS and JS containing my Projects and Blogs.",
+            githubLink: "https://github.com/kumarhites/portfolio",
+            liveLink: "https://kumarhitesh.netlify.app/"
+        },
+        {
+            id: "Mark 6",
+            projectName: "Minion Translator",
+            info: "A fun transalation app developed using Vanilla JS to transalate English language to Minion's Banana language.",
+            githubLink: "https://github.com/kumarhites/minion-translator",
+            liveLink: "https://minion-translatr.netlify.app/"
+        },
+        {
+            id: "Mark 7",
+            projectName: "Fictional Translator",
+            info: "An app to translate your input to a different language selected by user.",
+            githubLink: "https://github.com/kumarhites/fictional-translation",
+            liveLink: "https://fictional-translatr.netlify.app/"
+        },
+        {
+            id: "Mark 8",
+            projectName: "EmojiPedia 📚",
+            info: "React app to infer the meaning of most used emojis.",
+            githubLink: "https://github.com/kumarhites/emojipedia",
+            liveLink: "https://emojipdia.netlify.app/"
+        },
+        {
+            id: "Mark 9",
+            projectName: "GameJam",
+            info: "React app that has my favourite games, which I enjoyed playing.",
+            githubLink: "https://github.com/kumarhites/gamejam",
+            liveLink: "https://gamesjam.netlify.app/"
+        },
+        {
+            id: "Mark 10",
+            projectName: "Cash Register Manager",
+            info: "An application which outputs the amount change to be returned to the customer based on given cash and bill amount by the customer.",
+            githubLink: "",
+            liveLink: ""
+        },
+        {
+            id: "Mark 11",
+            projectName: "Is Your Birthday Lucky ?",
+            info: "This app takes user's DOB and lucky number and checks if his/her Birthday is lucky or not.",
+            githubLink: "https://github.com/kumarhites/luckybirthday",
+            liveLink: "https://luckybirthday0.netlify.app/"
+        },
+        {
+            id: "Mark 12",
+            projectName: "Fun With Triangles",
+            info: "Vanilla JS app to test your knowledge about Triangles. Have a look at it!",
+            githubLink: "https://github.com/kumarhites/fun-with-triangles",
+            liveLink: "https://funwithtriangles09.netlify.app/"
+        },
+        {
+            id: "Mark 13",
+            projectName: "Birthday Palindrome",
+            info: "Vanilla JS app to check if the enterd DOB is palindrome or not, if not it shows the nearest palindrome date, and by how many days it got missed.",
+            githubLink: "https://github.com/kumarhites/birthday-plaindrome",
+            liveLink: "https://bright-taffy.netlify.app/"
+        },
+        {
+            id: "Mark 14",
+            projectName: "Investing right?",
+            info: "Vanilla JS app to check if you have made profit or loss based on the current price and stocks purchased.",
+            githubLink: "https://github.com/kumarhites/stockProfitLoss",
+            liveLink: "https://sparkly-pixie-f8f1fb.netlify.app/"
+        },
+    ]
+}
